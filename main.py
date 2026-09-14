9398e0f852c4a1b45646f6d2008ac30d0e7f5ff2
Kis44KaPjj <twocattsinsong81@gmail.com>
2026-09-14 12:20:36 +0000

    Version 1.1.2 - BugFix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import socket
 import argparse
+import sys
 from __version__ import __version__
 from banner import BANNER
 
@@ -9,13 +10,20 @@ parser.add_argument('--version', action='version', version=f'NetSearch Version {
 parser.add_argument('domain', nargs='?', help='Domain to resolve')
 args = parser.parse_args()
 
-print(BANNER)
-print('=' * 48)
-
-domain = args.domain or input('\n\u276f Enter a domain: ').strip()
-
 try:
-    ip = socket.gethostbyname(domain)
-    print(f'[OK] IP for domain {domain} \u2192 {ip}')
-except socket.gaierror:
-    print(f'[ERROR] Domain not found: {domain}')
+    if not args.domain:
+        print(BANNER)
+        print('=' * 45)
+        
+        domain = args.domain or input('\n\u276f Enter a domain: ').strip()
+
+    try:
+        ip = socket.gethostbyname(domain)
+        print(f'[OK] IP for domain {domain} \u2192 {ip}')
+    except socket.gaierror:
+        print(f'[ERROR] Domain not found: {domain}')
+
+except KeyboardInterrupt:
+    print('\nCanceled by user.')
+    sys.exit(0)
+
